e5c8a04d37697375c68afea9c0e04a5d8f9b9be8
Javier Peletier <jm@epiclabs.io>
2026-06-02 11:29:31 +0200

    SR-001: add vecs network dependencies

---
+++ apps/server/src/network/vecsFoundation.ts
@@ -0,0 +1,17 @@
+import {
+  NetworkClient,
+  NetworkInput,
+  Networked,
+  ServerWorld,
+  VecsListener,
+  View,
+} from '@vworlds/vecs-server';
+
+export const vecsServerFoundationImports = {
+  NetworkClient,
+  NetworkInput,
+  Networked,
+  ServerWorld,
+  VecsListener,
+  View,
+};
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,4 +2,5 @@ export const commonPackageName = '@spacerocks/common';
 
 export * from './components';
 export * from './network/protocol';
+export * from './network/vecsFoundation';
 export * from './network/world';
